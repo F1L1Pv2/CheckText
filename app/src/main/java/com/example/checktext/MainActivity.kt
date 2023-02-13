f970a62c8ba91a9ab4e90527914d13cf51d9c185
student-11 <Igor30522@gmail.com>
2023-02-13 15:10:22 +0100

    added checkPass function

--- app/src/main/java/com/example/checktext/MainActivity.kt
+++ app/src/main/java/com/example/checktext/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.checktext
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var button = findViewById<Button>(R.id.login)
+        var firstPass = findViewById<EditText>(R.id.firstPassInput)
+        var secondPass = findViewById<EditText>(R.id.secondPassInput)
+        var warningText = findViewById<TextView>(R.id.Warningss)
+        var checkmark = findViewById<ImageView>(R.id.succesfull)
+
+        fun checkPass(firstPassword: String, secondPassword: String): Array<String> {
+            val errors = mutableListOf<String>()
+            if (firstPassword != secondPassword){
+                errors.add("Nie są takie same")
+            }
+
+            val specialCharRegex = "[^A-Za-z0-9]".toRegex()
+
+            if (!specialCharRegex.containsMatchIn(firstPassword)){
+                errors.add("Hasło musi posiadać przynajmniej jeden znak specialny")
+            }
+
+            val upperCaseRegex = "[A-Z]".toRegex()
+            val lowerCaseRegex = "[a-z]".toRegex()
+            val numberRegex = "[0-9]".toRegex()
+
+            if(!upperCaseRegex.containsMatchIn(firstPassword)){
+                errors.add("Hasło musi posiadać przynajmniej jedną dużą litere")
+            }
+
+            if(!lowerCaseRegex.containsMatchIn(firstPassword)){
+                errors.add("Hasło musi posiadać przynajmniej jedną małą litere")
+            }
+
+            if(!numberRegex.containsMatchIn(firstPassword)){
+                errors.add("Hasło musi posiadać przynajmniej jedną liczbe")
+            }
+
+            return errors.toTypedArray()
+        }
+
+
     }
 }
